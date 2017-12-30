a38b0c0b921a01d8c62dfd4a47181c23b6d306dc
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2017-12-29 17:30:46 -0800

    Display items in todolist

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import TodoList from "./TodoList.js";
+import TodoItems from "./TodoItems.js";
   
 var destination = document.querySelector("#container");
   
 ReactDOM.render(
     <div>
         <TodoList/>
+
     </div>,
     destination
 );
