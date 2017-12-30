0219341f54951a55a92ba6fae21208da03848fb8
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2017-12-29 17:12:25 -0800

    create array/add objects that contain text and key to array

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
  
 class TodoList extends Component {
+  constructor(props,context) {
+    super(props,context);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== " ") {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value = " ";
+    }
+
+    console.log(itemArray);
+
+    e.preventDefault(); //wont reload page and clear
+
+  }
+
+
+
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task">
+          <form onSubmit={this.addItem}>  
+            <input ref={(a) => this._inputElement = a}
+                placeholder="enter task">
             </input>
             <button type="submit">add</button>
           </form>
