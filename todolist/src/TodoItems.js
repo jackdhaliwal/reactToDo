14b9093306a780d56aa067e28ce28e686ed1fb4c
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2017-12-29 18:49:25 -0800

    delete items functionality

--- todolist/src/TodoItems.js
+++ todolist/src/TodoItems.js
@@ -8,9 +8,14 @@ class TodoItems extends Component {
 		this.createTasks = this.createTasks.bind(this);
 	}
 
-	createTasks(item) {
-		return <li key={item.key}>{item.text}</li>
+	delete(key) {
+		this.props.delete(key);
 	}
+
+	createTasks(item) {
+  return <li onClick={() => this.delete(item.key)} 
+              key={item.key}>{item.text}</li>
+}
 
 	render() {
 		var todoEntries = this.props.entries;
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(e) {
@@ -35,6 +36,17 @@ class TodoList extends Component {
 
   }
 
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function(item) {
+      return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+
+    });
+  }
+
 
 
 
@@ -49,7 +61,8 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items} />
+        <TodoItems entries={this.state.items}
+                    delete={this.deleteItem} />
       </div>
     );
   }
